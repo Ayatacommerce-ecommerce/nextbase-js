133c0c76be4bf4c1abcaf28c9aad0988573b2980
Ajil R <developer2.telnet@gmail.com>
2022-06-07 13:11:02 +0530

    3

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// price change all zeno button 
 var pricechange_zeno =  (Country_code,tokken) => { 
     console.log('pricechange_zeno', Country_code)
 //price change zenobuilder
@@ -125,3 +126,93 @@ currencyCode
      return  data11;
 }
 }
+
+
+//  product page zeno button change 
+var pricechange_zeno_product =  (Country_code,tokken,product_id) => { 
+    $('.zn-product-cart-button').each( async function (i, item) {
+           var productid = $(this).data('product-id');
+             console.log('mainproduct id', product_id)   
+               if(productid === product_id)
+                   {
+            var mainprice;
+            if(productid)
+                {
+           mainprice =  await getprice(productid)
+                }
+            var price = mainprice.price
+            var baseprice = mainprice.mainprice ? mainprice.mainprice : ''
+            console.log('productprice id', productid)
+               console.log('productprice s', mainprice)
+            $('.sticky_footer .zn-price').text(price);
+            $('.sticky_footer .zn-ori-price').text(baseprice).show();  
+            $('.zn-product-price').css({"visibility":"visible"}) 
+                       
+                       
+           // get 3 / productid
+                       
+            var Country_code = localStorage.getItem("Country_code");
+       var data11 = await fetch('/graphql', {
+               method: 'POST',
+               mode: 'cors',
+               headers: {
+                   'Content-Type': 'application/json',
+                   'Authorization': `Bearer ${tokken}`
+               },
+               body: JSON.stringify({
+                   query: `
+              query productById{
+    site {
+       product(entityId: ${productid}) {
+         id
+         entityId
+         name
+            prices {
+           price {
+             ...MoneyFields
+           }
+         
+           salePrice {
+             ...MoneyFields
+           }
+           retailPrice {
+             ...MoneyFields
+           }
+           saved {
+             ...MoneyFields
+           }
+         
+         }
+     }
+   currency(currencyCode: ${Country_code})
+       {
+         display
+         {
+           symbol
+         }
+       }
+   } 
+   }
+   
+   fragment MoneyFields on Money {
+     value
+     currencyCode
+   }
+   `
+           }),
+           })
+           .then(res => res.json())
+           .then(data11 => {
+               var price_code = data11.data.site.product.prices.price.value/3
+                   var curency_code = data11.data.site.product.prices.price.currencyCode
+                   var curency_symbol = data11.data.site.currency.display.symbol
+                   var price = curency_symbol + "" + (price_code).toFixed(2);
+               console.log('price_code',price_code)
+                $('.sticky_footer .zn-product .zn-text').text(`or 3 payments of ${price}`);
+               
+           })
+                   }
+       });
+       
+   
+   }
