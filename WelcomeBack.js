f761bbb1fa5ae6137df842ba72ffc01f8627728e
Ajil R <developer2.telnet@gmail.com>
2022-06-07 18:16:03 +0530

    4

--- WelcomeBack.js
+++ WelcomeBack.js
@@ -9,7 +9,16 @@ class WelcomeBack extends React.Component {
 
     render(){
         country_code = localStorage.getItem('Country_code')
+        country_sku = localStorage.getItem('Country_sku')
+        seller_type = localStorage.getItem('seller_type')
         console.log("react output",country_code)
+        if (country_code) {
+            $('.featerdproduct').each(async function(i, item) {
+                var sku = $(item).data('sku');
+                var prid = $(item).data('product-id');
+                console.log("react output 2",sku,prid)
+            })
+        }
         return(
             <>
                 <h2>Hello {this.state.name || 'Friend'}! Welcome Back.</h2>
